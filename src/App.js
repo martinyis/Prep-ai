b5939634e9f958446d3b8011ee67b3da18d42e92
martinyis <martinyis11@gmail.com>
2023-02-22 15:16:19 -0500

    Added theme function MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Switch } from 'react-router';
+import { createTheme } from '@mui/system';
 import HomePage from './pages/HomePage';
 import CreatePrep from './pages/CreatePrep';
 import ResultPage from './pages/ResultPage';
 import NavBar from './components/NavBar';
 import Layout from './components/Layout';
+const theme = createTheme({
+  pallete: {},
+});
 function App() {
   return (
     <BrowserRouter>
